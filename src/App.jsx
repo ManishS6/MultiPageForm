860101b411b5c3f836ad3d5b9abb63fe409f05a2
ManishS6 <contct.swami@yahoo.com>
2022-02-01 17:45:38 +0530

    updated file renames in imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Name from './Components/Name'
-import Address from './Components/Address'
+import Basic from './Components/Basic'
+import Education from './Components/Education'
 import Contact from './Components/Contact'
 import Review from './Components/Review'
-import Footer from './Components/Footer'
+import Resume from './Components/Resume'
 import Summary from './Components/Summary';
 import Skills from './Components/Skills';
 import Extra from './Components/Extra';
@@ -14,11 +14,11 @@ import { FormProvider } from './Components/FormContext';
 function App() {
   return (
       <FormProvider>
-        <div className="App">
+        <div className='App'>
           <Router>  
-            <Routes>  
-              <Route path="/" exact element={<Name/>}/>
-              <Route path="/address" element={<Address/>}/>
+            <Routes >  
+              <Route path="/" exact element={<Basic />}/>
+              <Route path="/page/education" element={<Education/>}/>
               <Route path="/contact" exact element={<Contact/>}/>
               <Route path="/skills" exact element={<Skills/>}/>
               <Route path="/summary" exact element={<Summary/>}/>
@@ -26,7 +26,7 @@ function App() {
               <Route path="/review" exact element={<Review/>}/>
               {/* <Route path="/shop/:id" element={Item} /> */}
             </Routes>
-            <Footer />
+            <Resume/>
           </Router>
         </div>
       </FormProvider>
